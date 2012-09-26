d08b072ab52ac0007f80bfae1609de29422f9a02
Sam Georges <sam@daftspunk.com>
2012-09-26 22:40:42 +1000

    Fixes load order issue, you can include framework in HEAD tags

--- javascript/foundation/app.js
+++ javascript/foundation/app.js
@@ -1,4 +1,4 @@
-;(function ($, window, undefined) {
+jQuery(document).ready(function($) { 
   'use strict';
 
   var $doc = $(document),
@@ -17,10 +17,10 @@
   $('input, textarea').placeholder();
 
   // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+  $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+  $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+  $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+  $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
 
   // Hide address bar on mobile devices
   if (Modernizr.touch) {
@@ -31,4 +31,4 @@
     });
   }
 
-})(jQuery, this);
+});
