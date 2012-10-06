909298a68a1c236b3d2b890a982def52a46d232f
Sam Georges <sam@daftspunk.com>
2012-10-06 14:59:21 +1000

    Update foundation JS

--- javascript/foundation/app.js
+++ javascript/foundation/app.js
@@ -1,20 +1,22 @@
-jQuery(document).ready(function($) { 
+;(function ($, window, undefined) {
   'use strict';
 
   var $doc = $(document),
       Modernizr = window.Modernizr;
 
-  $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-  $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-  $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-  $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-  $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-  $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
-  $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-  $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
-  $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+  $(document).ready(function() {
+    $.fn.foundationAlerts           && $doc.foundationAlerts();
+    $.fn.foundationButtons          && $doc.foundationButtons();
+    $.fn.foundationAccordion        && $doc.foundationAccordion();
+    $.fn.foundationNavigation       && $doc.foundationNavigation();
+    $.fn.foundationTopBar           && $doc.foundationTopBar();
+    $.fn.foundationCustomForms      && $doc.foundationCustomForms();
+    $.fn.foundationMediaQueryViewer && $doc.foundationMediaQueryViewer();
+    $.fn.foundationTabs             && $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup});
+    $.fn.foundationTooltips         && $doc.foundationTooltips();
 
-  $('input, textarea').placeholder();
+    $('input, textarea').placeholder();
+  });
 
   // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
   $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
@@ -22,8 +24,8 @@ jQuery(document).ready(function($) {
   $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
   $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
 
-  // Hide address bar on mobile devices
-  if (Modernizr.touch) {
+  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+  if (Modernizr.touch && !window.location.hash) {
     $(window).load(function () {
       setTimeout(function () {
         window.scrollTo(0, 1);
@@ -31,4 +33,4 @@ jQuery(document).ready(function($) {
     });
   }
 
-});
+})(jQuery, this);
